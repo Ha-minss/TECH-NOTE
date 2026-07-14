fc8ba4a72340eeead8593a7117f48c1377a40ef3
Dongha Park <ehdgk0725@gmail.com>
2026-07-14 23:44:59 +0900

    Delete amex-credit-risk-decisioning directory

--- amex-credit-risk-decisioning/src/modeling/predict.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import pandas as pd
-
-
-def predict_risk_scores(model, X: pd.DataFrame) -> pd.Series:
-    """Return ranking-oriented risk scores from a fitted binary classifier."""
-    return pd.Series(model.predict_proba(X)[:, 1], index=X.index, name="risk_score")
